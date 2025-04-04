1541066a2def063201e02356bc98a5fb2f7e96b5
Gustavo Henrique Pedroni da Silva <gugu2007.gp@gmail.com>
2025-04-04 09:01:29 -0300

    Update Aula 6 - EX4.py

--- Algoritmo e Lógica de Programação/Aula 6/Aula 6 - EX4.py
+++ Algoritmo e Lógica de Programação/Aula 6/Aula 6 - EX4.py
@@ -1,17 +1,14 @@
 a = int(input("Número inteiro: "))
 i = 0
 while i < a:
-    b = float(input())
+    b = float(input(f"Digite o {i + 1}º Elemento: "))
     if b > 0:
-        if i == 0:
-            menor = b
+        if i == 0 or b > maior:
             maior = b
-        if b > maior:
-            maior = b
-        if b < menor:
+        if i == 0 or b < menor:
             menor = b
+        i = i + 1
     else:
         print("Número Invalido")
-    i = i + 1
 print(f"O maior número {maior}")
 print(f"O menor número {menor}")
