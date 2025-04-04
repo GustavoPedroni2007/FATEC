0d98adc2ce0d4c862d3d060ebddc61c8b7846a55
Gustavo Henrique Pedroni da Silva <gugu2007.gp@gmail.com>
2025-04-04 08:49:11 -0300

    Update Aula 6 - EX3.py

--- Algoritmo e Lógica de Programação/Aula 6/Aula 6 - EX3.py
+++ Algoritmo e Lógica de Programação/Aula 6/Aula 6 - EX3.py
@@ -1,13 +1,10 @@
 a = int(input("Número inteiro: "))
 i = 0
 while i < a:
-    b = float(input())
-    if i == 0:
-        menor = b
+    b = float(input(f"Digite o {i + 1}º elemento: "))
+    if i == 0 or b > maior:
         maior = b
-    if b > maior:
-        maior = b
-    if b < menor:
+    if i == 0 or b < menor:
         menor = b
     i = i + 1
 print(f"O maior número {maior}")
